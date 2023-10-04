dcfcf0562a15e6853e7242d11276a4d66e9ad7e9
Benedict <benedictryan80@gmail.com>
2023-10-04 17:24:55 +0700

    2nd commit

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
-import Courses from './components/Courses';
+// import Courses from './components/Courses';
 
 export default function Home() {
 	return (
 		<div>
 			<p className="text-3xl text-center mb-7">App Router Next.js</p>
-			<Courses />
+			{/* <Courses /> */}
 		</div>
 	);
 }
