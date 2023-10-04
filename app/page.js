5b100bacc309e60d874a682789203aa6fd7e9eee
Benedict <benedictryan80@gmail.com>
2023-10-04 17:32:37 +0700

    add courses api

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
-// import Courses from './components/Courses';
+// 'use client';
+import Courses from './components/Courses';
 
 export default function Home() {
 	return (
 		<div>
 			<p className="text-3xl text-center mb-7">App Router Next.js</p>
-			{/* <Courses /> */}
+			<Courses />
 		</div>
 	);
 }
