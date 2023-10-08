20aa2de2b10cb471d2ab1ca2a5ae8d7f5ee585fb
Benedict <benedictryan80@gmail.com>
2023-10-08 16:33:51 +0700

    enable back prefetch

--- app/code/repos/page.js
+++ app/code/repos/page.js
@@ -40,7 +40,7 @@ const CodeRepos = async () => {
 						className="text-center max-w-2xl p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mb-5"
 					>
 						<Link
-							prefetch={false}
+							// prefetch={false}
 							href={`repos/${name}`}
 							className="hover:underline mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white"
 						>
