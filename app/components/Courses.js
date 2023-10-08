a3e3ab74e02b2f000e955382a5ba63ac5a289f2d
Benedict <benedictryan80@gmail.com>
2023-10-08 21:55:55 +0700

    implement responsiveness on main page

--- app/components/Courses.js
+++ app/components/Courses.js
@@ -18,23 +18,23 @@ const Courses = async () => {
 			{courses.map(({ id, title, description, link, level }) => (
 				<div
 					key={id}
-					className="mx-auto  w-9/12 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mb-5"
+					className="mx-auto  lg:w-9/12 md:w-9/12 w-full p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mb-5"
 				>
-					<p className="mt-1 mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+					<p className="mt-1 mb-2 lg:text-2xl md:text-2xl text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
 						{title}
 					</p>
-					<p className="text-lg mb-3 font-normal text-gray-500 dark:text-gray-400">
+					<p className="lg:text-lg md:text-lg text-md mb-3 font-normal text-gray-500 dark:text-gray-400">
 						{description}
 					</p>
 
 					<p className="mb-6 font-normal text-gray-500 dark:text-gray-400">
 						Level: {level}
 					</p>
-					<div className="mb-3">
+					<div className="lg:mb-3 md:mb-3 mb-1">
 						<Link
 							href={link}
 							target="_blank"
-							className="bg-blue-500  cursor-pointer p-3 rounded-md text-white font-normal"
+							className="bg-blue-500  cursor-pointer lg:p-3 md:p-3 p-2 rounded-md text-white font-normal"
 						>
 							Go to Course
 						</Link>
