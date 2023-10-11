4bd414045a97eb9c1b574665a717fe193c1afff7
Benedict <benedictryan80@gmail.com>
2023-10-11 17:12:51 +0700

    apple caching solutions

--- app/caching-solution/page.js
+++ app/caching-solution/page.js
@@ -1,11 +1,20 @@
 // export const dynamic = 'force-dynamic';
+'use client';
+
+import { useState, useEffect } from 'react';
 
 const getRandomNumber = () => {
 	return Math.random();
 };
 
 const CachingSolution = () => {
-	return <p className="text-5xl">{getRandomNumber()}</p>;
+	const [randomNumber, setRandomNumber] = useState('');
+
+	useEffect(() => {
+		setRandomNumber(String(getRandomNumber()) || 'Loading...');
+	}, []);
+
+	return <p className="text-5xl">{randomNumber}</p>;
 };
 
 export default CachingSolution;
