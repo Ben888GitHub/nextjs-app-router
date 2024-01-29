7641ecd4493eca8476f54bea8d7cb07daf53cff2
Benedict <benedictryan80@gmail.com>
2024-01-29 15:02:09 +0700

    .

--- app/libs/getHealthData.js
+++ app/libs/getHealthData.js
@@ -1,9 +1,15 @@
 export const getHealthData = async () => {
-	const response = await fetch(
-		'https://api.storyblok.com/v1/cdn/stories/health?version=published&token=wANpEQEsMYGOwLxwXQ76Ggtt'
-	);
-	await new Promise((resolve) => setTimeout(resolve, 750));
+	try {
+		const response = await fetch(
+			'https://api.storyblok.com/v1/cdn/stories/health?version=published&token=wANpEQEsMYGOwLxwXQ76Ggtt'
+		);
+		await new Promise((resolve) => setTimeout(resolve, 750));
 
-	const healthData = await response.json();
-	return healthData;
+		const healthData = await response.json();
+		return healthData;
+	} catch (error) {
+		// Handle error
+		console.error(error);
+		return { error };
+	}
 };
--- app/libs/getVueData.js
+++ app/libs/getVueData.js
@@ -1,9 +1,15 @@
 export const getVueData = async () => {
-	const response = await fetch(
-		'https://api.storyblok.com/v1/cdn/stories/vue?version=published&token=wANpEQEsMYGOwLxwXQ76Ggtt'
-	);
-	await new Promise((resolve) => setTimeout(resolve, 750));
+	try {
+		const response = await fetch(
+			'https://api.storyblok.com/v1/cdn/stories/vue?version=published&token=wANpEQEsMYGOwLxwXQ76Ggtt'
+		);
+		await new Promise((resolve) => setTimeout(resolve, 750));
 
-	const vueData = await response.json();
-	return vueData;
+		const vueData = await response.json();
+		return vueData;
+	} catch (error) {
+		// Handle error
+		console.error(error);
+		return { error };
+	}
 };
