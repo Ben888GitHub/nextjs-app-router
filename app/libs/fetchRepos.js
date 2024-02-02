55dcee01a0e01c495f31e024970401c6007fbade
Benedict <benedictryan80@gmail.com>
2024-02-02 21:05:26 +0700

    fetch github api with ssr to get updated data

--- app/libs/fetchRepos.js
+++ app/libs/fetchRepos.js
@@ -1,7 +1,11 @@
+// * https://blog.webdevsimplified.com/2024-01/next-js-app-router-cache/
 export const fetchRepos = async () => {
 	// * SSG with force-cache
 	const response = await fetch(
-		'https://api.github.com/users/bradtraversy/repos'
+		'https://api.github.com/users/bradtraversy/repos',
+		{
+			cache: 'no-store'
+		}
 	);
 
 	// await new Promise((resolve) => setTimeout(resolve, 500));
